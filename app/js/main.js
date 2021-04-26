39644f2499b71289bda126312b6ecea412ab4fda
adlid <baidildaevbahon08@gmail.com>
2021-04-27 02:23:39 +0600

    Add tarif plans

--- app/js/main.js
+++ app/js/main.js
@@ -79,3 +79,57 @@ changeModalState = (state) => {
 };
 changeModalState(modalState);
 const url = 'https://jsonplaceholder.typicode.com/users';
+
+
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.classes = classes;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+    }
+
+    changeToUAH() {
+        this.price = this.price * this.transfer;
+    }
+    render() {
+        const element = document.createElement('div');
+        if (this.classes.length === 0) {
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+        } else {
+            this.classes.forEach(className =>
+                element.classList.add(className));
+        }
+
+
+        element.innerHTML =
+
+            ` <img src= ${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider">   </div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+          `;
+        this.parent.append(element);
+    }
+}
+new MenuCard(
+
+    "img/tabs/vegy.jpg",
+    "vegy",
+    "MEnu Fitnes",
+    'Menu fites - ryo bla bla bla',
+    9,
+    '.menu .container',
+    'menu__item',
+    'big'
+
+).render();
